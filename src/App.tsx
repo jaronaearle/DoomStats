35a430ff2f35347c186c506b9b234653a1539c04
Jaron Earle <jaronaearle@gmail.com>
2023-03-10 23:19:23 -0700

    feat: initial strava auth prompt

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,18 @@
-import './App.css'
+import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      DoomStats!
-    </div>
-  )
-}
+const App = () => {
+	const stravaAuth =
+		'https://www.strava.com/oauth/authorize?client_id=<CLIENT_ID></CLIENT_ID>&response_type=code&redirect_uri=http://localhost:5173&approval_prompt=force&scope=read';
 
-export default App
+	return (
+		<>
+			<div className='App'>DoomStats!</div>
+			<section>
+				<p>Auth Strava</p>
+				<a href={stravaAuth}>Do it!</a>
+			</section>
+		</>
+	);
+};
+
+export default App;
