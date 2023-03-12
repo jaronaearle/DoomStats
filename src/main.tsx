4fbe04181045c51ab780b81d119694f351856425
Jaron Earle <jaronaearle@gmail.com>
2023-03-11 23:02:01 -0700

    add react router

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,23 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import './index.css';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+
+const router = createBrowserRouter([
+	{
+		path: '/',
+		element: <App redirected={false} />,
+	},
+	{
+		path: '/exchange_token',
+		element: <App redirected />,
+	},
+]);
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+	<React.StrictMode>
+		<RouterProvider router={router} />
+		{/* <App /> */}
+	</React.StrictMode>
+);
